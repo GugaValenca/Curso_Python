87e23c48b02dea4da1ae2b3986634a945c168f7c
GugaValenca <gustavo_valenca@hotmail.com>
2025-07-20 13:16:48 -0500

    Add initial implementation of personal information display with IMC calculation

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = 'Gustavo Valenca'
+altura = 1.80
+peso = 86.5
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é', imc)
+
+# Gustavo Valenca tem 1.80 de altura,
+# pesa 86.5 quilos e seu IMC é
+# 26.220379146919437
