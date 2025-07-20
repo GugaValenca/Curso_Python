3e095da7a486cfee38b703067c5dcb40256ec96e
GugaValenca <gustavo_valenca@hotmail.com>
2025-07-20 12:57:57 -0500

    Add basic arithmetic operations and their outputs

---
+++ aula9.py
@@ -0,0 +1,21 @@
+adicao = 10 +10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print('Subtração', subtracao)   
+
+multiplicacao = 10 *10
+print('Multiplicação', multiplicacao)   
+
+divisao = 10 / 2.2 # float
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2 # nao retorna as casas decimais
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10 # Significa 2 elevado a 10 ou 2 x 2 ... 10 vezes
+print('Exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão ==
+print('Módulo', modulo)
+
