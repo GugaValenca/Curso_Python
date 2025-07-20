3cb80011c79d7814b933ef7ed58e64e661817ea5
GugaValenca <gustavo_valenca@hotmail.com>
2025-07-20 13:30:14 -0500

    Classes about f-strings

---
+++ aula13.py
@@ -0,0 +1,17 @@
+nome = 'Gustavo Valenca'
+altura = 1.80
+peso = 86.5
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+# :,.2f = arredonda para 2 casas decimais e acrescenta uma virgula.
+linha_2 = f'pesa {peso:.2f} quilos e seu IMC é {imc:.2f}'
+linha_3 = f'{imc:.2f}'
+
+print(linha_1)
+print(linha_2)
+print(linha_3)
+
+# Gustavo Valenca tem 1.80 de altura,
+# pesa 86.5 quilos e seu IMC é
+# 26.220379146919437
