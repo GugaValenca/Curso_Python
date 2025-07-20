a5da2bb434c55c1a9067884f3d5d50f8fc786405
GugaValenca <gustavo_valenca@hotmail.com>
2025-07-20 13:33:05 -0500

    Update IMC display format in personal information output

--- aula13.py
+++ aula13.py
@@ -5,7 +5,7 @@ imc = peso / altura ** 2
 
 linha_1 = f'{nome} tem {altura:.2f} de altura'
 # :,.2f = arredonda para 2 casas decimais e acrescenta uma virgula.
-linha_2 = f'pesa {peso:.2f} quilos e seu IMC é {imc:.2f}'
+linha_2 = f'pesa {peso:.2f} quilos e seu IMC é'
 linha_3 = f'{imc:.2f}'
 
 print(linha_1)
@@ -14,4 +14,4 @@ print(linha_3)
 
 # Gustavo Valenca tem 1.80 de altura,
 # pesa 86.5 quilos e seu IMC é
-# 26.220379146919437
+# 26.697530864197528
