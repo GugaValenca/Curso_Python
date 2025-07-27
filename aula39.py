ee8a34926e640631953a625e637276370b1d9716
GugaValenca <gustavo_valenca@hotmail.com>
2025-07-27 13:45:35 -0500

    Refactor aula39.py to ensure asterisks are prefixed to each character in the name input

--- aula39.py
+++ aula39.py
@@ -4,7 +4,9 @@ indice = 0
 novo_nome = ''
 while indice < len(nome):
     letra = nome[indice]
-    novo_nome += letra + '*'
+    novo_nome += f'*{letra}'
     indice += 1
+
+novo_nome = novo_nome + '*'
 
 print(novo_nome)
