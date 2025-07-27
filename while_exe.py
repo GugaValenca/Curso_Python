c81dc436b4498e84cdcd2a7c9cddc0870375999b
GugaValenca <gustavo_valenca@hotmail.com>
2025-07-27 14:28:06 -0500

    Add while_exe.py for countdown functionality based on user input

---
+++ while_exe.py
@@ -0,0 +1,18 @@
+while True:
+
+    numero = input("Digite um número: ")
+
+    if not numero.isdigit():
+        print("Entrada inválida! Digite apenas números positivos.")
+    else:
+        numero = int(numero)
+
+        if numero < 0:
+            print("Número inválido, digite apenas números positivos.")
+        else:
+            # Executa a contagem regressiva até 0
+            while numero >= 0:
+                print(numero)
+                numero -= 1  # Decrementa o número em 1 a cada iteração
+
+print("Fim da contagem")
