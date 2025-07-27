c34b5f4802ce74fb0960aa92666f6c16419b87a2
GugaValenca <gustavo_valenca@hotmail.com>
2025-07-27 12:14:26 -0500

    Add initial implementation of string formatting examples in Format_Strings.py

---
+++ Format_Strings.py
@@ -0,0 +1,18 @@
+#Formatação de strings
+
+preco = 1234.567
+print(f"Preço normal: {preco}")
+print(f"Preço formatado: {preco:.2f}")       # duas casas decimais
+print(f"Preço com vírgula: {preco:,.2f}")   # separador de milhar
+
+
+import locale
+
+# Define a localização para Brasil
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+
+valor = 1234567.89
+
+# Usa a formatação de moeda padrão do Brasil
+print(f"Valor formatado no padrão brasileiro: {locale.currency(valor, grouping=True)}")
+# Exibe o valor formatado com separador de milhar e símbolo de moeda
